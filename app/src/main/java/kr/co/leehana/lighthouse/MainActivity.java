89dc06b299416de7a7cc859439418131137f5204
Hana Lee <i@leehana.co.kr>
2015-12-05 15:01:55 +0900

    저장소 초기화

---
+++ app/src/main/java/kr/co/leehana/lighthouse/MainActivity.java
@@ -0,0 +1,10 @@
+package kr.co.leehana.lighthouse;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * @author Hana Lee
+ * @since 2015-12-05 14:45.
+ */
+public class MainActivity extends FragmentActivity {
+}
